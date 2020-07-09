bd02e126ed3dfc7b4bab7cbc08e69157a681f592
Matheus Rufino <contato@matheusrufino.com.br>
2020-07-09 18:54:18 -0300

    Create SignIn and SignUp pages

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,122 @@
+import React, { useCallback, useRef } from 'react';
+import { Image, KeyboardAvoidingView, View, Platform, ScrollView, TextInput, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+
+import { useAuth } from '../../hooks/auth';
+import logoImg from '../../assets/logo.png';
+
+import { Container, CreateAccountButton, CreateAccountButtonText, Title, ForgotPassword, ForgotPasswordText } from './styles';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const { signIn } = useAuth();
+
+  const handleSignIn = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail is required.')
+          .email('Invalid e-mail format.'),
+        password: Yup.string()
+          .required('Password is required.'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await signIn({
+        email: data.email,
+        password: data.password
+      });
+
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert('Authentication error', 'Check your credentials and try again');
+    }
+  }, [signIn]);
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>
+                Sign In your account
+            </Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus()
+                }}
+              />
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Password"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
+              <Button onPress={() => formRef.current?.submitForm()}>Access</Button>
+            </Form>
+
+            <ForgotPassword onPress={(() => { console.log('btn') })}>
+              <ForgotPasswordText>Forgot password?</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={(() => navigation.navigate('SignUp'))}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
+  )
+}
+
+export default SignIn;
